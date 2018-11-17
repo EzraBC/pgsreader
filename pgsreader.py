3a2eab75e67a964e26536e32c763db2ed0749736
Ezra <ezrabc@gmail.com>
2018-11-16 22:00:36 -0600

    Add files via upload

--- pgsreader.py
+++ pgsreader.py
@@ -1,9 +1,6 @@
 class InvalidSegmentException(Exception):
     '''raised when a segment does not match PGS specification'''
 
-def i(b, s, l):
-    return int(b[s:s+l].hex(), base=16)
-
 class PGSReader:
 
     def __init__(self, filepath):
@@ -14,20 +11,18 @@ class PGSReader:
         cls = SEGMENT_TYPE[bytes_[10]]
         return cls(bytes_)
     
-    @classmethod
-    def get_segments(cls, bytes_):
+    def get_segments(self, bytes_):
         segments = []
         while bytes_:
             size = 13 + int(bytes_[11:13].hex(), base=16)
-            segments.append(bytes_[:size])
+            segments.append(self.make_segment(bytes_[:size]))
             bytes_ = bytes_[size:]
         return segments
 
     @property
     def segments(self):
         if not hasattr(self, '_segments'):
-            seg_bytes = self.get_segments(self.bytes)
-            self._segments = [self.make_segment(s) for s in seg_bytes]
+            self._segments = self.get_segments(self.bytes)
         return self._segments
 
 
@@ -45,10 +40,10 @@ class BaseSegment:
         self.bytes = bytes_
         if bytes_[:2] != b'PG':
             raise InvalidSegmentException
-        self._pts = i(bytes_, 2, 4)/90
-        self._dts = i(bytes_, 6, 4)/90
+        self._pts = int(bytes_[2:6].hex(), base=16)/90
+        self._dts = int(bytes_[6:10].hex(), base=16)/90
         self._type = self.SEGMENT[bytes_[10]]
-        self._size = i(bytes_, 11, 2)
+        self._size = int(bytes_[11:13].hex(), base=16)
         self.data = bytes_[13:]
 
     def __len__(self):
@@ -81,16 +76,16 @@ class PresentationCompositionSegment(BaseSegment):
 
         def __init__(self, bytes_):
             self.bytes = bytes_
-            self.object_id = i(bytes_, 0, 2)
+            self.object_id = int(bytes_[0:2].hex(), base=16)
             self.window_id = bytes_[2]
             self.cropped = bool(bytes_[3])
-            self.x_offset = i(bytes_, 4, 2)
-            self.y_offset = i(bytes_, 6, 2)
+            self.x_offset = int(bytes_[4:6].hex(), base=16)
+            self.y_offset = int(bytes_[6:8].hex(), base=16)
             if self.cropped:
-                self.crop_x_offset = i(bytes_, 8, 2)
-                self.crop_y_offset = i(bytes_, 10, 2)
-                self.crop_width = i(bytes_, 12, 2)
-                self.crop_height = i(bytes_, 14, 2)
+                self.crop_x_offset = int(bytes_[8:10].hex(), base=16)
+                self.crop_y_offset = int(bytes_[10:12].hex(), base=16)
+                self.crop_width = int(bytes_[12:14].hex(), base=16)
+                self.crop_height = int(bytes_[14:16].hex(), base=16)
 
     STATE = {
         int('0x00', base=16): 'Normal',
@@ -100,10 +95,10 @@ class PresentationCompositionSegment(BaseSegment):
 
     def __init__(self, bytes_):
         BaseSegment.__init__(self, bytes_)
-        self.width = i(self.data, 0, 2)
-        self.height = i(self.data, 2, 2)
+        self.width = int(self.data[0:2].hex(), base=16)
+        self.height = int(self.data[2:4].hex(), base=16)
         self.frame_rate = self.data[4]
-        self._num = i(self.data, 5, 2)
+        self._num = int(self.data[5:7].hex(), base=16)
         self._state = self.STATE[self.data[7]]
         self.palette_update = bool(self.data[8])
         self.palette_id = self.data[9]
@@ -138,10 +133,10 @@ class WindowDefinitionSegment(BaseSegment):
     def __init__(self, bytes_):
         BaseSegment.__init__(self, bytes_)
         self.window_id = self.data[0]
-        self.x_offset = i(self.data, 1, 2)
-        self.y_offset = i(self.data, 3, 2)
-        self.width = i(self.data, 5, 2)
-        self.height = i(self.data, 7, 2)
+        self.x_offset = int(self.data[1:3].hex(), base=16)
+        self.y_offset = int(self.data[3:5].hex(), base=16)
+        self.width = int(self.data[5:7].hex(), base=16)
+        self.height = int(self.data[7:9].hex(), base=16)
 
 class PaletteDefinitionSegment(BaseSegment):
 
@@ -177,13 +172,13 @@ class ObjectDefinitionSegment(BaseSegment):
     
     def __init__(self, bytes_):
         BaseSegment.__init__(self, bytes_)
-        self.id = i(self.data, 0, 2)
+        self.id = int(self.data[0:2].hex(), base=16)
         self.version = self.data[2]
         self.in_sequence = self.SEQUENCE[self.data[3]]
-        self.data_len = i(self.data, 4, 3)
-        self.width = i(self.data, 7, 2)
-        self.height = i(self.data, 9, 2)
-        self.img_data = self.data[11:]
+        self.data_len = int(self.data[4:7].hex(), base=16)
+        self.width = int(self.data[7:9].hex(), base=16)
+        self.height = int(self.data[9:11].hex(), base=16)
+        self.img_data = self.data[7:]
         if len(self.img_data) != self.data_len:
             print('Warning: Image data length asserted does not match the '
                   'length found.')
